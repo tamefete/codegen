599ab39bfdb642b5a7935374f5efe7161ebfd208
TamPM <tampm.fat@gmail.com>
2017-06-06 18:02:33 +0700

    Add Db project

---
+++ CodeGenerator/POCOWriter/POCOWriterFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using Db;
+
+namespace CodeGenerator.POCOWriter
+{
+    public abstract class POCOWriterFactory
+    {
+        public abstract IPOCOWriter CreatePOCOWriter();
+    }
+
+    public class RichTextBoxWriterFactory : POCOWriterFactory
+    {
+        private RichTextBox rtb;
+
+        public RichTextBoxWriterFactory(RichTextBox rtb)
+        {
+            this.rtb = rtb;
+        }
+
+        public override IPOCOWriter CreatePOCOWriter()
+        {
+            return new RichTextBoxPOCOWriter(rtb);
+        }
+    }
+
+    public class StringBuilderWriterFactory : POCOWriterFactory
+    {
+        public StringBuilder sb;
+
+        public StringBuilderWriterFactory(StringBuilder sb)
+        {
+            this.sb = sb;
+        }
+
+        public override IPOCOWriter CreatePOCOWriter()
+        {
+            return new StringBuilderPOCOWriter(sb);
+        }
+    }
+
+}
